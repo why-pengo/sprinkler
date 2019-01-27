892d6c022ddc1f9b5671dd2d656b3510fa757ac6
Jon Morgan <morgan.jon@gmail.com>
2019-01-27 15:39:14 -0500

    adding zonemap info from old project

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,6 +1,20 @@
 from django.db import models
 
-# Zone Map
+
 class zone(models.Model):
+    """Zone Map"""
+    num = models.IntegerField()
     pin = models.IntegerField()
+    bcm = models.IntegerField()
+    gpio = models.IntegerField()
+    # map zone number to BCM number
+    #              BCM   pin
+    # zoneMap[1] = 17  # 11 gpio0
+    # zoneMap[2] = 18  # 12 gpio1
+    # zoneMap[3] = 27  # 13 gpio2
+    # zoneMap[4] = 22  # 15 gpio3
+    # zoneMap[5] = 23  # 16 gpio4
+    # zoneMap[6] = 24  # 18 gpio5
+    # zoneMap[7] = 25  # 22 gpio6
+    # zoneMap[8] = 4   # 7 gpio7
 
