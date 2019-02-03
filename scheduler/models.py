8205a0f959e7b6b3aad2f9ce246f45b6f71f7608
Jon Morgan <morgan.jon@gmail.com>
2019-02-03 16:45:23 -0500

    remove unneeded comments

--- scheduler/models.py
+++ scheduler/models.py
@@ -4,18 +4,10 @@ from django.db import models
 class ZoneMap(models.Model):
     """
     Zone Map
-           zone  BCM  pin gpio
-    zoneMap[1] = 17  # 11 gpio0
-    zoneMap[2] = 18  # 12 gpio1
-    zoneMap[3] = 27  # 13 gpio2
-    zoneMap[4] = 22  # 15 gpio3
-    zoneMap[5] = 23  # 16 gpio4
-    zoneMap[6] = 24  # 18 gpio5
-    zoneMap[7] = 25  # 22 gpio6
-    zoneMap[8] = 4   # 7 gpio7
     """
     num = models.IntegerField()
     bcm = models.IntegerField()
     pin = models.IntegerField()
     gpio = models.IntegerField()
+
 
