d011ad49400601bb5ad2e8881607486ed7e4d013
Jon Morgan <morgan.jon@gmail.com>
2019-02-03 19:11:28 -0500

    adding some tracing to logging

--- scheduler/apps.py
+++ scheduler/apps.py
@@ -8,14 +8,17 @@ class SchedulerConfig(AppConfig):
         from .utils import init_zone_map, get_current_zone_map, print_zone_map, gpio_setup
         from .models import ZoneMap
 
-        print(f"Checking if zone map is setup.")
-        zone_one = ZoneMap.objects.all()
-        if len(zone_one) > 0:
-            print(f"It is already initialized.")
-        else:
-            print(f"It is not, initializing zone map.")
-            init_zone_map()
-        zone_map = get_current_zone_map()
-        print_zone_map(zone_map)
-        print(f"Calling gpio_setup.")
-        gpio_setup(zone_map)
+        print(f"apps::ready: Checking if zone map is setup.")
+        try:
+            zone_one = ZoneMap.objects.all()
+            if len(zone_one) > 0:
+                print(f"apps::ready: It is already initialized.")
+            else:
+                print(f"apps::ready: It is not, initializing zone map.")
+                init_zone_map()
+            zone_map = get_current_zone_map()
+            print_zone_map(zone_map)
+            print(f"apps::ready: Calling gpio_setup.")
+            gpio_setup(zone_map)
+        except Exception as e:
+            print(f"apps::ready: Exception: {e}")
