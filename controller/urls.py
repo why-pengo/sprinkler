bcac518f973fde31a5f7fadd9cac3b42f1e7dfd9
Jon Morgan <morgan.jon@gmail.com>
2019-02-24 10:24:57 -0500

    Have login/out working with my dtl template

--- controller/urls.py
+++ controller/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.contrib.auth.views import LoginView
-from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 from status.views import HomeView
 
 urlpatterns = [
     # Django
     path('admin/', admin.site.urls),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', LoginView.as_view(template_name='accounts/login.html')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
     # sprinkler
-    path('', login_required(HomeView.as_view())),
+    path('', HomeView.as_view()),
 ]
--- status/views.py
+++ status/views.py
@@ -1,9 +1,31 @@
 from django.views import View
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+# from django.contrib.auth import authenticate, login, logout
 
 
 class HomeView(View):
 
     @staticmethod
+    @login_required()
     def get(request):
         return render(request, 'index.html', None)
+
+
+# class LoginView(View):
+#
+#     @staticmethod
+#     def post(request):
+#         print(f"scheduler/views::post: entering.")
+#         username = request.POST['username']
+#         password = request.POST['password']
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             print(f"scheduler/views::post: user authenticated.")
+#             login(request, user)
+#             # redirect
+#             return redirect(request.GET.get('next'))
+#         else:
+#             print(f"scheduler/views::post: user not authenticated.")
+#             # Return an 'invalid login' error message.
+#             pass
