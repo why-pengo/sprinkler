9e2460009b22a422900a2675659b0c24919da9e9
Jon Morgan <morgan.jon@gmail.com>
2019-03-02 10:42:33 -0500

    use StaticLiveServerTestCase to get pytest and selenium working

--- scheduler/tests/views_test.py
+++ scheduler/tests/views_test.py
@@ -1,15 +1,26 @@
-from selenium import webdriver
-import unittest
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+import os
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 
-class NewVisitorTest(unittest.TestCase):
-    def setUp(self):
-        self.browser = webdriver.Firefox()
+class NewVisitorTests(StaticLiveServerTestCase):
 
-    def tearDown(self):
-        self.browser.quit()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
 
     def test_landing_page_title(self):
-        self.browser.get('http://localhost:8000')
+        self.selenium.get(f"{self.live_server_url}")
 
-        self.assertIn('Sprinkler Controller ][', self.browser.title)
+        self.assertIn('Sprinkler Controller ][', self.selenium.title)
