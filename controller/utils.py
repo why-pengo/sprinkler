351dc9f33c0db29585a6d3f0c7b7ace8297c85b0
Jon Morgan <morgan.jon@gmail.com>
2019-03-04 20:36:49 -0500

    untested state!!!

--- controller/utils.py
+++ controller/utils.py
@@ -1,5 +1,8 @@
 import sys
 import platform
+
+from celery.utils.serialization import jsonify
+
 from scheduler.models import ZoneMap
 from _datetime import datetime, timedelta
 
@@ -124,9 +127,90 @@ def relay_call(pin, call):
 
     # zone = list(zoneMap.keys())[list(zoneMap.values()).index(pin)]
     zone = ZoneMap.objects.filter(pin__exact=int(pin))
-    print(f"scheduler/utils::relay_call: zone = {zone} pin = {pin} call = {call} 0/On 1/Off time = {datetime.now}")
+    print(f"controller/utils::relay_call: zone = {zone} pin = {pin} call = {call} 0/On 1/Off time = {datetime.now}")
     if 'wiringpi' in sys.modules:
         value = wiringpi.digitalRead(pin)  # Read pin
         wiringpi.digitalWrite(pin, call)  # Write (1 = HIGH/OFF, 0 = LOW/ON ) to pin
         value = wiringpi.digitalRead(pin)  # Read pin
 
+
+# API backend calls
+def zone_on(zone):
+    zone_map = ZoneMap.objects.get(num__exact=zone)
+    print(f"controller/utils::zoneOn: zone = {zone}")
+    print(f"controller/utils::zoneOn: BCM  = {zone_map.bcm}")
+    run_for(zone, 5)
+    return jsonify('zoneOn: %d' % zone)
+
+
+def zone_off(zone):
+    zone_map = ZoneMap.objects.get(num__exact=zone)
+    print(f"controller/utils::zoneOff: zone = {zone}")
+    print(f"controller/utils::zoneOff: BCM = {zone_map.bcm}")
+    relay_call(zone_map.bcm, 1)
+    return jsonify('zoneOff: %d' % zone)
+
+
+def run_for(zone, minutes):
+    zone_map = ZoneMap.objects.get(num__exact=zone)
+    bcm = zone_map.bcm
+    print(f"controller/utils::runFor: zone = {zone}")
+    print(f"controller/utils::runFor: minutes = {minutes}")
+    print(f"controller/utils::runFor: BCM = {bcm}")
+
+    # schedule start
+    # start_time = datetime.now() + timedelta(seconds=20)
+    print(f"controller/utils::run_for: adding relay_call({bcm}, 0) start now")
+    # replace with celery beat
+    # scheduler.add_job(relay_call, args=[bcm, 0])
+
+    # schedule stop
+    stop_time = datetime.now() + timedelta(minutes=minutes)
+    print("runFor: adding relay_call(", zoneMap[zone], ", 1) ", stop_time)
+    # logging.debug("runFor: adding relay_call(%d, 1 - on stop now", zoneMap[zone])
+    # replace with celery beat
+    # scheduler.add_job(relay_call, 'date', run_date=stop_time, args=[bcm, 1])
+
+    return jsonify('runFor: %d' % zone)
+
+
+# def list_jobs():
+#     job_list = []
+#     jobs = scheduler.get_jobs()
+#     for i in range(0, len(jobs)):
+#         job = {}
+#         job['name'] = jobs[i].name
+#         args = jobs[i].args
+#         if len(args) != 0:
+#             job['arg0'] = args[0]
+#             job['arg1'] = args[1]
+#         job['trigger'] = str(jobs[i].trigger)
+#         print("list_jobs: found job ", job)
+#         job_list.append(job)
+#
+#     return jsonify(job_list)
+#
+#
+# def time_stamp():
+#     print("Time stamp: ", datetime.now())
+#
+#
+# def running():
+#     print("running: check if any zone is currently running")
+#     value = 0
+#     for i in range(1, len(zoneMap)):
+#         if 'wiringpi' in sys.modules:
+#             value = wiringpi.digitalRead(zoneMap[i])  # Read pin
+#         else:
+#             value = 1
+#
+#         if(not value):
+#             OnOff = "On"
+#             print("running: zone = ", i, ", pin = ", zoneMap[i], " is ", OnOff)
+#             return jsonify(i)
+#         else:
+#             OnOff = "Off"
+#
+#         print("running: zone = ", i, ", pin = ", zoneMap[i], " is ", OnOff)
+#
+#     return jsonify('none')
