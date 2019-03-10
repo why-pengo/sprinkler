8f5be53db74ba8ae3bdfee8c4968f735fd56c1ad
Jon Morgan <morgan.jon@gmail.com>
2019-03-09 22:22:05 -0500

    working test on schedular api response

--- scheduler/tests/scheduler_views_test.py
+++ scheduler/tests/scheduler_views_test.py
@@ -1,30 +1,30 @@
-# from django.contrib.staticfiles.testing import StaticLiveServerTestCase
-# from selenium.webdriver.firefox.webdriver import WebDriver
-# import json
-# import os
-#
-# SCREEN_DUMP_LOCATION = os.path.join(
-#     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
-# )
-#
-#
-# class SchedulerViewsTests(StaticLiveServerTestCase):
-#
-#     @classmethod
-#     def setUpClass(cls):
-#         super().setUpClass()
-#         cls.selenium = WebDriver()
-#         cls.selenium.implicitly_wait(10)
-#
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.selenium.quit()
-#         super().tearDownClass()
-#
-#     def test_rest_zonemap_render_page(self):
-#         self.selenium.get(f"{self.live_server_url}/zones/?format=json")
-#         self.selenium.save_screenshot(f"{SCREEN_DUMP_LOCATION}/test_rest_zonemap_render_page.png")
-#         json_data = json.loads(self.selenium.find_element_by_tag_name('body').text)
-#
-#         self.assertIn('"num": 1', json_data)
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+import requests
+import json
+import os
+
+SCREEN_DUMP_LOCATION = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
+
+
+class SchedulerViewsTests(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
+    def test_rest_zonemap_api_call(self):
+        response = requests.get(f"{self.live_server_url}/zones/?format=json")
+        json_data = response.json()
+
+        self.assertEqual(200, response.status_code)
 
