e9e21867b44451934d59f6e11f4f16cae276fa55
Jon Morgan <morgan.jon@gmail.com>
2019-09-21 14:54:38 -0400

    move cron commands to shell script

---
+++ scripts/cron_run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit
+fi
+
+if [ -z "$1" ]
+  then
+    echo "No argument supplied Django base dir. Argument number 1."
+fi
+base=$1
+
+if [ -z "$2" ]
+  then
+    echo "No argument supplied zone. Argument number 2."
+fi
+zone=$2
+
+if [ -z "$3" ]
+  then
+    echo "No argument supplied switch (on/off). Argument number 3."
+fi
+switch=$3
+
+cd "$base" || exit
+
+venv=$(pipenv --venv)
+venv_python=$venv/bin/python
+installed_pipenv=$(command -v pipenv)
+
+"$installed_pipenv" run "$venv_python" manage.py zoneOnOff "$zone" "$switch"
