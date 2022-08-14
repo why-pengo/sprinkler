a51b18423eebc63632245870c84ecaa3367bf4e2
Jon Morgan <morgan.jon@gmail.com>
2022-08-14 09:28:08 -0400

    working on new create/delete schedule flow WIP

--- static/js/schedule.js
+++ static/js/schedule.js
@@ -1,24 +1,27 @@
 function ready() {
-    console.log(`id = ${id}`);
-    console.log(`dow = ${dow}`);
-    console.log(`start = ${start}`);
-    console.log(`end = ${end}`);
-    console.log(`active = ${active}`);
-    console.log(`run_once = ${run_once}`);
-    console.log(`zone = ${zone}`);
-
-    document.getElementById("id_dow").value = dow;
-    document.getElementById("id_start").value = start;
-    document.getElementById("id_end").value = end;
-    document.getElementById("id_zone").value = zone;
-    if (active === "True") {
-        document.getElementById("id_active").checked = true;
-    } else {
-        document.getElementById("id_active").checked = false;
-    }
-    if (run_once === "True") {
-        document.getElementById("id_run_once").checked = true;
-    } else {
-        document.getElementById("id_run_once").checked = false;
-    }
+    // for edits, may not do edits.
+    // console.log(`id = ${id}`);
+    // console.log(`dow = ${dow}`);
+    // console.log(`start = ${start}`);
+    // console.log(`end = ${end}`);
+    // console.log(`active = ${active}`);
+    // console.log(`run_once = ${run_once}`);
+    // console.log(`zone = ${zone}`);
+    // console.log(`crontab = ${crontab}`);
+    // console.log(`cron_key = ${cron_key}`);
+    //
+    // document.getElementById("id_dow").value = dow;
+    // document.getElementById("id_start").value = start;
+    // document.getElementById("id_end").value = end;
+    // document.getElementById("id_zone").value = zone;
+    // if (active === "True") {
+    //     document.getElementById("id_active").checked = true;
+    // } else {
+    //     document.getElementById("id_active").checked = false;
+    // }
+    // if (run_once === "True") {
+    //     document.getElementById("id_run_once").checked = true;
+    // } else {
+    //     document.getElementById("id_run_once").checked = false;
+    // }
 }
